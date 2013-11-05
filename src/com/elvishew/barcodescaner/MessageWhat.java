e05c4ec4dc905d655a0fd4f1f8ee91260d7af40c
qiuyongan <qiuyongan@wiselinksz.com>
2013-11-05 18:12:09 +0800

    First commit.

---
+++ src/com/elvishew/barcodescaner/MessageWhat.java
@@ -0,0 +1,15 @@
+package com.elvishew.barcodescaner;
+
+public class MessageWhat {
+    public static final int AUTO_FOCUS=0x7f060000;
+    public static final int DECODE=0x7f060001;
+    public static final int DECODE_FAILED=0x7f060002;
+    public static final int DECODE_SUCCEEDED=0x7f060003;
+    public static final int ENCODE_FAILED=0x7f060004;
+    public static final int ENCODE_SUCCEEDED=0x7f060005;
+    public static final int LAUNCH_PRODUCT_QUERY=0x7f060006;
+    public static final int PREVIEW_VIEW=0x7f06000c;
+    public static final int QUIT=0x7f060007;
+    public static final int RESTART_PREVIEW=0x7f060008;
+    public static final int RETURN_SCAN_RESULT=0x7f060009;
+}
